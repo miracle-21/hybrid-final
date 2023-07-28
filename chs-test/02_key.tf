78a793a49443f42b2d08ca3b1525edf4161d66fe
Thomasmichaelshelby <114216018+Thomasmichaelshelby@users.noreply.github.com>
2023-07-28 10:52:01 +0900

    Update 02_key.tf

--- chs-test/02_key.tf
+++ chs-test/02_key.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "hbinT_key" {
   key_name   = "chs-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDDMOBFTJYEZtJfOiZXVmOsUM32e9+VJM56MShpFmHHNpGxeYDg3wxZOt8yRKF0rJYlalABqeNKlJBAAbwPPllkZ2W5TNyQS/ED1P5tl9nrl+caO3Gbt5LhqVBtbjEwHS4rlm7j9UQkAXjVnypSkZIpsTW6tBy3+A1zk71jpeGQ90dOT/r9d068rJJx72dpa6uhFDGxN+OlWZPa7jSGCXdWHAqOEoAG9IrwptpZQAOQFmfOpSVRefaYfL+1+hJAlmOQyqvYZdRHoDTubaa5TJF7as0XVkkxl9sU3KvWVOattfL9f9LIwzBQhKZB1fy9i28DhMw2gjgBzoZV5ssF6eVVvr0woJh31eXbUFnyh6XUfvAkB3vZdp+YY5v+P0Lhyo2se6J63sfc5R1glCjXctiFdgNuDYHK32yrgSvERGeSmOj23iBS5WicexOQSGP4qEaC5jjOngrIKgiqOCoKuRTGg+mkB/3qUmGj/Ot6x20hH7toK6njIJm423gaCQz0Kk8="
+  public_key = ""
 }
 
 resource "aws_key_pair" "hbinT_key1" {
