c23827c1a126cd4e0a0a4ef820e65c524ce794d8
Thomasmichaelshelby <114216018+Thomasmichaelshelby@users.noreply.github.com>
2023-08-02 11:56:16 +0900

    Update 03_sub.tf

--- chs-test/03_sub.tf
+++ chs-test/03_sub.tf
@@ -5,7 +5,10 @@ resource "aws_subnet" "hbinT_pub_" {
   availability_zone = "${var.region}${count.index == 0 ? "a" : "c"}"
 
   tags = {
-    Name = "${var.tag}-pub-${count.index == 0 ? "a" : "c"}"
+    Name                              = "${var.tag}-pub-${count.index == 0 ? "a" : "c"}"
+    "kubernetes.io/cluster/4team-clu" = "shared"
+    "kubernetes.io/role/internal-elb" = 1
+    "kubernetes.io/role/elb"          = 1
   }
 }
 
@@ -15,7 +18,10 @@ resource "aws_subnet" "hbinT_pri_" {
   cidr_block        = "10.100.${count.index == 0 ? "2" : "3"}.0/24"
   availability_zone = "${var.region}${count.index == 0 ? "a" : "c"}"
   tags = {
-    Name = "${var.tag}-pri-${count.index == 0 ? "a" : "c"}"
+    Name                              = "${var.tag}-pri-${count.index == 0 ? "a" : "c"}"
+    "kubernetes.io/cluster/4team-clu" = "shared"
+    "kubernetes.io/role/internal-elb" = 1
+    "kubernetes.io/role/elb"          = 1
   }
 }
 
@@ -25,6 +31,9 @@ resource "aws_subnet" "hbinT_db_" {
   cidr_block        = "10.100.${count.index == 0 ? "4" : "5"}.0/24"
   availability_zone = "${var.region}${count.index == 0 ? "a" : "c"}"
   tags = {
-    Name = "${var.tag}-db-${count.index == 0 ? "a" : "c"}"
+    Name                              = "${var.tag}-db-${count.index == 0 ? "a" : "c"}"
+    "kubernetes.io/cluster/4team-clu" = "shared"
+    "kubernetes.io/role/internal-elb" = 1
+    "kubernetes.io/role/elb"          = 1
   }
 }
