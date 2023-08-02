3488f2341925f5514da87f49256a8d88d4ddb17c
P-MH <miraculum95@gmail.com>
2023-08-02 11:03:12 +0900

    pmh-terraform

---
+++ pmh-terraform/05.sg.tf
@@ -0,0 +1,78 @@
+# EKS 보안그룹
+resource "aws_security_group" "eks_secu" {
+  name   = "${var.name}-cluster"
+  vpc_id = aws_vpc.vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.rocidr]
+  }
+
+  ingress = [
+    {
+      description      = "ssh"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = [var.rocidr]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = null
+      security_groups  = null
+      self             = null
+    },
+    {
+      description      = "https"
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      cidr_blocks      = [var.rocidr]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = null
+      security_groups  = null
+      self             = null
+    }
+  ]
+
+  tags = {
+    Name = "${var.name}-cluster"
+  }
+}
+
+# DB 보안그룹
+# resource "aws_security_group" "db_secu" {
+#   name   = "${var.name}-db"
+#   vpc_id = aws_vpc.vpc.id
+
+#   ingress {
+#     from_port   = 3306
+#     to_port     = 3306
+#     protocol    = "tcp"
+#     cidr_blocks = [var.rocidr]
+#   }
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = [var.rocidr]
+#   }
+#   tags = {
+#     Name = "${var.name}-db"
+#   }
+# }
+
+# EFS
+resource "aws_security_group" "efs_secu" {
+  name   = "${var.name}-efs"
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    description = "nfs"
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "TCP"
+    cidr_blocks = [var.rocidr]
+  }
+}
